a507a0cfff80de6bf7ab1688bf9e00791493cece
saci zakaria <sacideveloppeur@gmail.com>
2023-02-02 09:25:18 +0100

    Future(info)

--- test.dart
+++ test.dart
@@ -1,19 +1,12 @@
 void main() {
-  final f1 = Future(() {
-    print(" task 1");
-    info();
-  });
+  final f1 = Future(info);
 
-  print("task 2");
+  print("task 1");
 }
 
-void info() {
-  print("task 3");
+double info() {
+  return 1000;
 }
 
 // OUTPOUT
-// Connecting to VM Service at http://127.0.0.1:56739/QFzMIEz0vZE=/
-// task 2
-//  task 1
-// task 3
-// Exited
+// task 1
